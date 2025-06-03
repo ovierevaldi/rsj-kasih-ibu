bce89ab72457b283bc1a5d3ff299da7ccfbcc7e6
Ovie Revaldi <ovierevaldi@gmail.com>
2025-06-03 14:16:56 +0700

    Update UI for Homepage

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -12,17 +12,25 @@ const HomePage = () => {
   }
 
   return (
-    <div>
-      <div>Welcome To</div>
-      <div>RSJ Kasih Ibu</div>
+    <div className="flex flex-col h-screen items-center justify-center gap-8" >
+      <div className="mb-2">
+        <div className="text-5xl text-center mb-4">Welcome To</div>
+        <div className="text-5xl text-center">RSJ Kasih Ibu</div>
+      </div>
 
-      <div className="flex gap-x-8">
-        <button className="bg-[#125DFF] text-white p-4 rounded-lg" 
+      <div className="flex gap-x-12 text-2xl">
+        <button 
+          className="bg-[#125DFF] text-white p-4 rounded-lg min-w-56 hover:bg-[#0f4dbb] transition-colors duration-300 cursor-pointer" 
           onClick={() =>  goToRegistration()}
-        >Pendaftaran</button>
-        <button className="bg-[#125DFF] text-white p-4 rounded-lg"
-            onClick={() =>  goToJadwalPengobatan()}
-        >Jadwal Pengobatan</button>
+          >
+            Pendaftaran
+        </button>
+        <button 
+          className="bg-[#125DFF] text-white p-4 rounded-lg min-w-56 hover:bg-[#0f4dbb] transition-colors duration-300 cursor-pointer"
+          onClick={() =>  goToJadwalPengobatan()}
+        >
+          Jadwal Pengobatan
+        </button>
       </div>
     </div>
   )
